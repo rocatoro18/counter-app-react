8d734936776f7915f61b1a939cf59d030e55138c
Roberto Torres <59813315+rocatoro18@users.noreply.github.com>
2023-09-15 16:56:57 -0700

    click event

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -1,11 +1,21 @@
 import PropTypes from 'prop-types'
 
+
 export const CounterApp = ({value})=>{
+
+    // Funcion de flecha
+    const handleAdd = (event,newValue) => {
+        //console.log(event)
+        console.log(newValue)
+    }
 
     return (
         <>
         <h1>Counter App</h1>
-        <h2>{value}</h2>
+        <h2> { value } </h2>
+        <button onClick={(event) => handleAdd(event,'Hola')}>
+            1+
+        </button>
         </>
     );
 
