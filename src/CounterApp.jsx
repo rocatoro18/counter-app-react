7235c25a5bedaf6eaa02e937e9cee007ae130ce0
Roberto Torres <59813315+rocatoro18@users.noreply.github.com>
2023-09-15 17:24:25 -0700

    useState Hook

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -1,19 +1,20 @@
 import PropTypes from 'prop-types'
-
+import {useState} from 'react'
 
 export const CounterApp = ({value})=>{
 
-    // Funcion de flecha
-    const handleAdd = (event,newValue) => {
-        //console.log(event)
-        console.log(newValue)
+    const [counter, setCounter] = useState(value)
+    
+    const handleAdd = () => {
+       //setCounter(counter+1);
+       setCounter((c)=>c + 1 )
     }
 
     return (
         <>
         <h1>Counter App</h1>
-        <h2> { value } </h2>
-        <button onClick={(event) => handleAdd(event,'Hola')}>
+        <h2> { counter } </h2>
+        <button onClick={handleAdd}>
             1+
         </button>
         </>
